1140569c16efdad8a8459535c81a4fc775f375bb
inditabrituraj <rituraj.kumar@inditab.in>
2024-09-23 11:08:00 +0530

    created the skeleon for 4 routes in controller

--- app/Http/Controllers/API/APIcontroller.php
+++ app/Http/Controllers/API/APIcontroller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+
+
 use App\Models\User;
 
 
@@ -10,5 +12,21 @@ use Illuminate\Http\Request;
 
 class APIcontroller extends Controller
 {
+    // we will use user table of the default db to create the user
+    //this method will hit by post method which will contain [email and password] 
+    public function register(Request $request){
+        
+    }
+    //post method which will contain the registerd email and password and will return the api key
+    public function login(Request $request){
+
+    }
+    //these 2 are protected method and need token
+    public function profile(Request $request){
+
+    }
+    public function logout(Request $request){
+
+    }
     
 }
