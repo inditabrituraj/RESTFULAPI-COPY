2379c8bc014b18b00e7d562e7197e675f160e6ae
inditabrituraj <rituraj.kumar@inditab.in>
2024-09-23 11:01:47 +0530

    created the controller and config the db

---
+++ app/Http/Controllers/API/APIcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class APIcontroller extends Controller
+{
+    
+}
