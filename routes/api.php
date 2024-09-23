bcd360c4a983ea0afa6f8930a3eb7915265f44ce
inditabrituraj <rituraj.kumar@inditab.in>
2024-09-23 11:30:55 +0530

    setup the routes for the api and added the middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+//import the controller
+use App\Http\Controllers\API\APIcontroller;
+
+Route::post('/login',[APIcontroller::class,'login']);
+Route::post('/register',[APIcontroller::class,'register']);
+
+Route::group([
+    "middleware"=>['auth:api']
+].function(){
+    Route::get('/profile',[APIcontroller::class,'profile']);
+    Route::get('/logout',[APIcontroller::class,'logout']);
+});
